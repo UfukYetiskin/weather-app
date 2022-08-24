f8a8a2773a5c160375575a9894c93b6c10b8d495
uyetiskin <ufukyetiskin@hotmail.com>
2022-08-24 14:46:01 +0300

    created store page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +16,9 @@ function App() {
         >
           Learn React
         </a>
+        Filizciğim
       </header>
+      
     </div>
   );
 }
